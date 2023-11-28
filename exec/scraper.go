9bc7d9dcd485ee3cce7a6baa29714c66ec17727f
deBarbarinAntoine <antoine.debarbarin@ynov.com>
2023-11-28 22:40:47 +0100

    Fixing line resizing function and CLI design

--- exec/scraper.go
+++ exec/scraper.go
@@ -6,6 +6,7 @@ import (
 	termsize "github.com/kopoli/go-terminal-size"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -25,6 +26,24 @@ type LightNovel struct {
 	Status string
 }
 
+type DisplayParameters struct {
+	InvariableColor string
+	LoadingBarColor string
+	ArrowColor      string
+	VariableColor   string
+	AlertColor      string
+}
+
+var myDisplayParams = DisplayParameters{
+	InvariableColor: utils.ColorCode(utils.Orange),
+	LoadingBarColor: utils.ColorCode(utils.Teal),
+	ArrowColor:      utils.ColorCode(utils.Aquamarine),
+	VariableColor:   utils.ColorCode(utils.Coral),
+	AlertColor:      utils.ColorCode(utils.Orangered),
+}
+
+var myLightNovels Library
+
 func clearCurrentLine() {
 	fmt.Printf("\033[0;") // clear current line
 	fmt.Printf("\033[2K\r%d", 0)
@@ -35,7 +54,7 @@ func clearCurrentLine() {
 
 func loadingBar(current, max, space int) string {
 	percentage := (float32(current) / float32(max)) * float32(space)
-	progressBar := utils.ColorCode(utils.Teal) + strings.Repeat("█", int(percentage)) + strings.Repeat("░", space-int(percentage)) + utils.CLEARCOLOR
+	progressBar := strings.Repeat("█", int(percentage)) + strings.Repeat("░", space-int(percentage))
 	return progressBar
 }
 
@@ -43,7 +62,7 @@ func resize(current, max int, invariable, variable string) string {
 	var result string
 	size, err := termsize.GetSize()
 	if err != nil {
-		return utils.ColorCode(utils.Aquamarine) + invariable + utils.CLEARCOLOR + " " + utils.ColorCode(utils.Deepskyblue) + variable + utils.CLEARCOLOR
+		return myDisplayParams.InvariableColor + invariable + " " + myDisplayParams.VariableColor + variable + utils.CLEARCOLOR
 	} else {
 		var progressBar string
 		width := size.Width
@@ -54,18 +73,18 @@ func resize(current, max int, invariable, variable string) string {
 		}
 		lenMsg := len([]rune(fmt.Sprint(invariable, " ", progressBar, " > ", variable)))
 		if len([]rune(variable)) <= -(width - lenMsg) {
-			result = invariable + " " + variable
+			result = myDisplayParams.InvariableColor + invariable + utils.CLEARCOLOR + " " + myDisplayParams.VariableColor + variable + utils.CLEARCOLOR
 		} else {
-			result = invariable + " " + progressBar + " > " + variable
+			result = myDisplayParams.InvariableColor + invariable + utils.CLEARCOLOR + " " + myDisplayParams.LoadingBarColor + progressBar + utils.CLEARCOLOR + myDisplayParams.ArrowColor + " > " + utils.CLEARCOLOR + myDisplayParams.VariableColor + variable + utils.CLEARCOLOR
 		}
-		if rest := width - len([]rune(result)); rest < 0 {
-			result = string([]rune(result)[:width])
+
+		colorRegExp := regexp.MustCompile("\\033\\[[0-9;]+m")
+		nbColors := len(colorRegExp.FindAllString(result, -1))
+		str := strings.Join(colorRegExp.Split(result, -1), "")
+
+		if rest := width - len([]rune(str)); rest < 0 {
+			result = string([]rune(result)[:len([]rune(result))+rest-nbColors])
 		}
-		arrowIndex := strings.Index(result, ">")
-		if arrowIndex == -1 {
-			return utils.ColorCode(utils.Aquamarine) + result + utils.CLEARCOLOR
-		}
-		result = utils.ColorCode(utils.Aquamarine) + result[:arrowIndex] + utils.ColorCode(utils.Cyan) + result[arrowIndex:arrowIndex+1] + utils.ColorCode(utils.Deepskyblue) + result[arrowIndex+1:] + utils.CLEARCOLOR
 		return result
 	}
 }
@@ -125,22 +144,32 @@ func (lib *Library) fetchLN() {
 	fmt.Println()
 }
 
-var myLightNovels Library
+func instructions() {
+	fmt.Println(myDisplayParams.ArrowColor, "Welcome to myLnLibraryCrawler!", utils.CLEARCOLOR)
+	fmt.Println(myDisplayParams.InvariableColor, "Miscellaneous commands (type it and press [Enter]):", utils.CLEARCOLOR)
+	fmt.Println(myDisplayParams.AlertColor, "     'stop' -> exit the program", utils.CLEARCOLOR)
+	fmt.Println(myDisplayParams.VariableColor, "     'skip' -> go to the next part of the program", utils.CLEARCOLOR)
+	fmt.Println(myDisplayParams.InvariableColor, "  [Any other input will show the library's current content]", utils.CLEARCOLOR)
+}
 
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	lib := &myLightNovels
+	instructions()
 	go lib.fetchLN()
 	for {
 		var input string
 		fmt.Scanln(&input)
 		if input == "stop" {
+			os.Exit(0)
+		} else if input == "skip" {
 			break
 		} else {
 			fmt.Println()
 			fmt.Printf("My LightNovels library: %#v", myLightNovels)
 			fmt.Println()
+			instructions()
 		}
 	}
 	wg.Wait()
